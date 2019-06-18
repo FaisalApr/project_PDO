03dfae861ef95efa96ebede556c5f95031cc3091
wildanal2-mac <wildanal2@gmail.com>
2019-06-18 13:57:09 +0700

    template main

---
+++ application/views/template.php
@@ -0,0 +1,45 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>DeskApp Dashboard</title>
+
+	<!-- Site favicon -->
+	<!-- <link rel="shortcut icon" href="images/favicon.ico"> -->
+
+	<!-- Mobile Specific Metas -->
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+	<!-- Google Font -->
+	<link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet">
+	<!-- CSS -->
+	<link rel="stylesheet" href="<?php echo base_url() ?>assets/vendors/styles/style.css">
+
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119386393-1"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+
+	  gtag('config', 'UA-119386393-1');
+	</script>
+</head>
+<body>
+<?php $this->load->view('header/header'); ?>
+<?php $this->load->view('header/sidebar'); ?>
+ 
+<div class="main-container">
+	<div class="pd-ltr-20 customscroll customscroll-10-p height-100-p xs-pd-20-10">
+		
+		
+
+	</div>
+</div>
+
+</body>
+<script src="<?php echo base_url() ?>assets/vendors/scripts/script.js"></script>
+</html>
